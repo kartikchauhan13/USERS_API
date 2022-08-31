385f1f549c580ae6d7505ca5e566d05071d09e8f
kartikchauhan13 <chauhankartikkc13@gmail.com>
2022-08-31 14:09:37 +0530

    commit 1

--- myWebsite/API/models.py
+++ myWebsite/API/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    User_id=models.AutoField(primary_key=True)
+    id=models.AutoField(primary_key=True)
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
     company_name=models.CharField(max_length=100)
     age=models.PositiveIntegerField(default=15)
     city=models.CharField(max_length=50)
     state=models.CharField(max_length=50)
-    Zip=models.CharField(max_length=50)
-    Email=models.EmailField()
+    zip=models.CharField(max_length=50)
+    email=models.EmailField()
     web=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.first_name
 
 
--- myWebsite/myWebsite/urls.py
+++ myWebsite/myWebsite/urls.py
@@ -1,7 +1,7 @@
-"""users_api URL Configuration
+"""myWebsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from API.views import UserList,UserDetail
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users',UserList.as_view() , name="user_list"),
+    path('api/users/<int:pk>' ,UserDetail.as_view() , name="user_detail"),
+    path('',TemplateView.as_view(template_name='home.html'))
+
 ]
